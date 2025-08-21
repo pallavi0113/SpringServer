595d61453b270a48966bfe8c61b6c8a82eeda1d7
Pallavi Kumari <paalavikr028@gmail.com>
2025-08-21 21:22:58 +0530

    new code

---
+++ ChallengeApp/src/test/java/com/SpringServe/ChallengeApp/ChallengeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringServe.ChallengeApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
