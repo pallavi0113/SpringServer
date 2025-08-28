c4c0c803078aae54adde97213c78741d63290664
Pallavi Kumari <paalavikr028@gmail.com>
2025-08-28 20:03:24 +0530

    resolved

--- src/main/java/com/ioc/coupling/IOCExample.java
+++ src/main/java/com/ioc/coupling/IOCExample.java
@@ -5,22 +5,23 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class IOCExample {
     public static void main(String[] arg){
-        ApplicationContext context=new ClassPathXmlApplicationContext("applicationIoCLooseCouplingExample.xml");
+        ApplicationContext context;
+        context = new ClassPathXmlApplicationContext("applicationIoCLooseCouplingExample.xml");
 
 
-        UserManager userManagerWithDB=
-                (UserManager) context.getBean("userManagerWithUserDataProvider");
+        UserManager userManagerWithDB;
+        userManagerWithDB = (UserManager) context.getBean("userManagerWithUserDataProvider");
         System.out.println(userManagerWithDB.getUserInfo());
 
 
-        UserManager userManagerWithWS=
-                (UserManager) context.getBean("userManagerWithWebServiceDataProvider");
+        UserManager userManagerWithWS;
+        userManagerWithWS = (UserManager) context.getBean("userManagerWithWebServiceDataProvider");
         System.out.println(userManagerWithWS.getUserInfo());
 
 
 
-        UserManager userManagerWithNewDB=
-                (UserManager) context.getBean("userManagerWithNewDatabaseProvider");
+        UserManager userManagerWithNewDB;
+        userManagerWithNewDB = (UserManager) context.getBean("userManagerWithNewDatabaseProvider");
         System.out.println(userManagerWithNewDB.getUserInfo());
     }
 }
